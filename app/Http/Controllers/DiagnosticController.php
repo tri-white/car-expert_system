14532574eed1c25ea49b18d3649fb73c3e599f97
tri-white <olegovic101@gmail.com>
2023-10-01 22:13:27 +0300

    added some logic into controller

--- app/Http/Controllers/DiagnosticController.php
+++ app/Http/Controllers/DiagnosticController.php
@@ -4,27 +4,75 @@ namespace App\Http\Controllers;
 
 use App\Symptom;
 use App\Malfunction;
+use App\DiagnosticRule;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DiagnosticController extends Controller
 {
     public function index()
-{
-    // Get a random symptom from the database
-    $randomSymptom = Symptom::inRandomOrder()->first();
+    {
+        // Get a random symptom from the database
+        $randomSymptom = Symptom::inRandomOrder()->first();
     
-    // Pass the random symptom data to the view
-    return view('diagnose', [
-        'randomSymptom' => $randomSymptom->name,
-        'symptomId' => $randomSymptom->id,
-    ]);
-}
-
-public function diagnose(Request $request)
-{
-    // Handle form submission and user answers here
-    // You can access the selected answer using $request->input('answer')
-    // Implement the logic to filter malfunctions based on user answers
-    // Redirect to the next question or display results when done
-}
+        // Pass the random symptom data to the view
+        return view('diagnose', [
+            'randomSymptom' => $randomSymptom->name,
+            'symptomId' => $randomSymptom->id,
+        ]);
+    }
+    
+    public function diagnose(Request $request)
+    {
+        $answer = $request->input('answer');
+        $symptomId = $request->input('symptom_id');
+    
+        // Initialize an empty list of malfunctions
+        $filteredMalfunctions = Malfunction::all();
+    
+        // Filter malfunctions based on the user's answer
+        if ($answer === 'yes') {
+            // Remove malfunctions that don't have the selected symptom
+            $filteredMalfunctions = $this->filterMalfunctionsBySymptom($filteredMalfunctions, $symptomId);
+        } elseif ($answer === 'no') {
+            // Remove malfunctions that have the selected symptom
+            $filteredMalfunctions = $this->filterMalfunctionsWithoutSymptom($filteredMalfunctions, $symptomId);
+        }
+    
+        // Check if there are more questions to ask
+        if ($this->areMoreQuestionsToAsk()) {
+            // Get a new random symptom
+            $randomSymptom = Symptom::inRandomOrder()->first();
+    
+            // Pass the random symptom data to the view
+            return view('diagnose', [
+                'randomSymptom' => $randomSymptom->name,
+                'symptomId' => $randomSymptom->id,
+            ]);
+        } else {
+            // Display the final results with the filtered malfunctions
+            return view('results', ['filteredMalfunctions' => $filteredMalfunctions]);
+        }
+    }
+    
+    private function filterMalfunctionsBySymptom($malfunctions, $symptomId)
+    {
+        return $malfunctions->filter(function ($malfunction) use ($symptomId) {
+            return $malfunction->symptoms->contains('id', $symptomId);
+        });
+    }
+    
+    private function filterMalfunctionsWithoutSymptom($malfunctions, $symptomId)
+    {
+        return $malfunctions->filter(function ($malfunction) use ($symptomId) {
+            return !$malfunction->symptoms->contains('id', $symptomId);
+        });
+    }
+    
+    private function areMoreQuestionsToAsk()
+    {
+        // Implement your logic to determine if there are more questions to ask
+        // For example, you can check if there are more symptoms or diagnostic rules to apply
+        // Return true if there are more questions, false otherwise
+    }
 }
