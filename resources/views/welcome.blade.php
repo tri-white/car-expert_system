8396f59440874132c78beaaad9880a7dac6b82e2
tri-white <olegovic101@gmail.com>
2023-10-01 22:22:06 +0300

    added configure page, not implemented yet. translated welcome blade to ukrainian

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,10 @@
 
 @section('content')
 <div class="container">
-    <h1>Welcome to the Vehicle Diagnostic Expert System</h1>
+    <h1>Вітаємо в експертній системі з діагностики автомобіля"</h1>
     <div class="options">
-        <a href="{{ route('diagnose') }}" class="btn btn-primary">Diagnose Car</a>
-        <a href="{{ route('configure') }}" class="btn btn-secondary">Configure Expert System</a>
+        <a href="{{ route('diagnose') }}" class="btn btn-primary">Пройти діагностику</a>
+        <a href="{{ route('configure') }}" class="btn btn-secondary">Конфігурація експертної системи</a>
     </div>
 </div>
 @endsection
