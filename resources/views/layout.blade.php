a31dae31a5bf9a7b6a3d72f2ada8485830d0757a
tri-white <olegovic101@gmail.com>
2023-10-02 09:15:17 +0300

    fixed welcome view load

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,11 +3,8 @@
   	<title>Діагностика автомобіля</title>
   </head>
   <body>
-  	@include('layout.header')
  
   		@yield('content')
- 
-  	@include('layout.footer')
  
   	@stack('js')
   </body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
 <div class="container">
     <h1>Вітаємо в експертній системі з діагностики автомобіля"</h1>
     <div class="options">
-        <a href="{{ route('diagnose') }}" class="btn btn-primary">Пройти діагностику</a>
+        <a href="{{ route('diagnose') }}" class="btn btn-primary">Пройти діагностику</a><br>
         <a href="{{ route('configure') }}" class="btn btn-secondary">Конфігурація експертної системи</a>
     </div>
 </div>
