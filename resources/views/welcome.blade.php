d8ed205fa1c9fc6ed7669ec0129b2c86503f3779
tri-white <olegovic101@gmail.com>
2023-10-03 22:07:53 +0300

    design fix of welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,8 @@
 @extends('layout')
 
 @section('content')
-<div class="container">
-    <h1>Вітаємо в експертній системі з діагностики автомобіля"</h1>
+<div class="container mt-5">
+    <h1 class="text-center">Вітаємо в експертній системі з діагностики автомобіля</h1>
     
 </div>
 @endsection
